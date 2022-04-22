bf20933b70b076abca7a9000c001519259143a4b
Gurparkash-Singh <singhgk.fateh@gmail.com>
2022-04-22 02:40:22 -0400

    Created react boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path")
+
+module.exports = {
+    entry: "./components/app.js",
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: "bundle.js"
+    },
+    module : {
+        rules: [{
+            loader: "babel-loader",
+            test: /\.js$/,
+            exclude: /node_modules/
+        },
+        {
+            test: /\.scss$/,
+            use: [
+                "style-loader",
+                "css-loader",
+                "sass-loader"
+            ]
+        },
+    ]
+    },
+    devtool: "eval-cheap-module-source-map",
+    devServer: {
+        static: './public',
+        port: 3000
+    }
+}
